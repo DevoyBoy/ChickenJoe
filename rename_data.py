ace5e82bc698758ef9713f56032238d7301e04d3
billyjnr <willreid144@gmail.com>
2023-02-09 11:37:19 +1000

    updated rename_data

--- rename_data.py
+++ rename_data.py
@@ -7,8 +7,6 @@ counter = 0
 for image in os.listdir(folder_dir):
     if image.endswith(".jpg"):
         counter += 1
-        # print("old name: " + image)
-
         # rename image
         if len(image.split('-')) == 2:    # has '-'
             new_name = str(counter) + '-' + image.split('-')[1]
@@ -18,4 +16,5 @@ for image in os.listdir(folder_dir):
             new_name = str(counter) + image[len(image)-8:]
             new_name = new_name.rjust(14, '0')
 
-        print(new_name)
+        print(image + "\t---> " + new_name)
+        # TODO: rename image
